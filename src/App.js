0f0783a2b7a59b82b33c810328210ac8758bdfca
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-07-29 22:19:06 +0530

    Added Basic Styling To City Card

--- src/App.js
+++ src/App.js
@@ -22,6 +22,12 @@ const App = () => {
                 onChange={(e) => setQuery(e.target.value)}
                 onKeyPress={search}
             />
+            {weather.main && (
+                <div className="city">
+                    <h2 className="city-name">
+                        <span>{weather.name}</span> <sup>{weather.sys.country}</sup></h2>
+                </div>
+            )}
         </div>
     )
 }
