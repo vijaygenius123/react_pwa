1773d8769ac1693e70bbcc7de38351810667ab31
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-07-29 22:07:32 +0530

    Calling Weather API on Enter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { fetchWeather } from './api/fetchWeather.js'
 
 const App = () => {
+    const [query, setQuery] = useState('')
+    const [weather, setWeather] = useState({})
+    const search = async (e) => {
+        if (e.key === 'Enter') {
+            const data = await fetchWeather(query);
+            setWeather(data)
+            setQuery('')
+        }
+    }
     return (
-        <h1>App</h1>
+        <div className="container">
+            <input type="text"
+                className="search"
+                placeholder="Search"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyPress={search}
+            />
+        </div>
     )
 }
 
--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -14,4 +14,4 @@ const fetchWeather = async (query) => {
     return data
 }
 
-export const { fetchWeather }
+export { fetchWeather }
