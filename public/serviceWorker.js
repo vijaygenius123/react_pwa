d9571dfac3514808bf59a14b41beed685b75cae6
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-07-30 23:27:31 +0530

    Added Listeing For Requests

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -4,11 +4,21 @@ const urlsTocache = ["index.html", "offline.html"]
 const self = this;
 
 
-this.addEventListener('install', (event) => {
+self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => {
                 console.log('Opened Cache');
                 return cache.addAll(urlsTocache);
             }))
+})
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request)
+                    .catch(() => { caches.match('offline.html') })
+            })
+    )
 })
