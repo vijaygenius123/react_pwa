f9763f879f4d560019bef8abb6e991b38769644e
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-07-30 23:30:51 +0530

    Added cache activate

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -22,3 +22,18 @@ self.addEventListener('fetch', (event) => {
             })
     )
 })
+
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = []
+    cacheWhitelist.push(CACHE_NAME)
+
+    event.waitUntil(
+        caches.keys().then((cacheNames => Promise.all(
+            cacheNames.map(cacheName => {
+                if (!cacheWhitelist.includes(cacheName))
+                    return caches.delete(cacheName)
+            })
+        )))
+    )
+})
